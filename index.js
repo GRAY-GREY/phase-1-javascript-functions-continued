b30beda3cd6a70f079309c26ea5329fc3da8f406
+ <+>
2021-10-16 23:21:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(wrap = '*') {
+    const part1 = "You are";
+    return function (adj = 'special') {
+        return `${part1} ${wrap}${adj}${wrap}!`;
+    }
+}
